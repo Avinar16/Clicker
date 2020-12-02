5d72b84971abb51ab219deee66ff5a02e7cbb62c
PROlodki <72821917+PROlodki@users.noreply.github.com>
2020-12-02 11:36:45 +0300

    Minor file movement and adding a button class

--- Scenes/Fight_scene.py
+++ Scenes/Fight_scene.py
@@ -12,5 +12,8 @@ class Fight_scene:
         click_button = pygame.Rect(200, 870, 250, 100)
         pygame.draw.rect(screen, [26, 240, 150], click_button)
         pygame.draw.rect(screen, [0, 0, 0], attack_button)
+        jump_button = pygame.Surface((100, 50))
+        jump_button.fill([206, 255, 0])
+        screen.blit(jump_button, coords)
         #draw_text(fone, str(score), 100, 1350, 100)
 
--- Scenes/Speedwagon_scene.py
+++ Scenes/Speedwagon_scene.py
@@ -2,7 +2,5 @@ import pygame
 
 class Speedwagon_scene:
     def __init__(self, screen, coords):
-        jump_button = pygame.Surface((100, 50))
-        jump_button.fill([206, 255, 0])
-        screen.blit(jump_button, coords)
 
+        pass
