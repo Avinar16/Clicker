2fb0ea8ad67f776c26ecc63ffebeedfbf59f3aa3
Andrew <omego1606@gmail.com>
2020-12-02 14:05:41 +0300

    Added Score_Counter

--- Classes/Score_Counter.py
+++ Classes/Score_Counter.py
@@ -1,7 +1,30 @@
 import pygame
+from Classes.draw_text import draw_text
+
 
 class Score_Counter:
-    def __init__(self, screen, cords):
-        Score_surfase = pygame.Surface((200, 200))
-        Score_surfase.fill([255, 255, 255])
-        screen.blit(Score_surfase, cords)
+    def __init__(self, screen):
+        # Reading score from file
+        with open('Classes\config.txt', mode='r', encoding='utf-8') as f:
+            self.coins = int(f.readline())
+
+        self.screen = screen
+        # Rendering score
+        draw_text(self.screen, str(self.coins), 64, 500, 100)
+
+
+
+    def add_coin(self):
+        self.coins += 1
+
+        print(self.coins)
+        with open('Classes\config.txt', mode='w', encoding='utf-8') as f:
+            f.write(str(self.coins))
+
+
+    def cleanup(self):
+        with open('Classes\config.txt', mode='w', encoding='utf-8') as f:
+            f.write(str(0))
+
+
+
