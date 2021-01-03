58599ad3451f6b3db46528b490ed41b1ab6e01d0
PROlodki <72821917+PROlodki@users.noreply.github.com>
2021-01-03 15:26:06 +0300

    New config file

--- Config.py
+++ Config.py
@@ -2,15 +2,14 @@ import csv
 
 
 class Config:
-    def __init__(self):
-        self.filename = 'config.csv'
+    def __init__(self, filename):
+        self.filename = filename
         self.config = None
-        self.readConfig()
 
-    def readConfig(self):
+    def readConfig(self, index=0):
         with open(self.filename, 'r', encoding='utf-8') as config_file:
             reader = csv.DictReader(config_file, delimiter=';', quotechar='"')
-            self.config = list(reader)[0]
+            self.config = list(reader)[index]
 
     def saveConfig(self):
         with open(self.filename, 'w', encoding='utf-8', newline='') as config_file:
@@ -23,5 +22,6 @@ class Config:
         if need_save_to_file:
             self.saveConfig()
 
-    def getValue(self, param_name):
+    def getValue(self, param_name, index=0):
+        self.readConfig(index)
         return self.config[param_name]
